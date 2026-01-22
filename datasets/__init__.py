f2af45150597f856abde1e345fd14d421438246f
Manda Kausthubh <manda.kausthubh@gmail.com>
2026-01-22 11:55:58 +0530

    added split datasets to init file

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -7,3 +7,4 @@ from .imagenet_a import ImageNetAValClasses, ImageNetA
 from .imagenet_r import ImageNetRValClasses, ImageNetR
 from .objectnet import ObjectNetValClasses, ObjectNet
 from .imagenet_sketch import ImageNetSketch
+from .imagenet_split import *
